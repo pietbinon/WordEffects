ee3769cd8eb48d7cd19dcbff44a23a7eb861f435
pietbinon <pierrebinon@icloud.com>
2017-02-14 17:51:25 -0500

    Initial Commit

---
+++ WordEffects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WordEffects
+//
+//  Created by Pierre Binon on 2017-02-14.
+//  Copyright © 2017 Pierre Binon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
